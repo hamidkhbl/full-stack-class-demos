97559c779fa50cccf0c9bd9f12e65534caae8f59
Hamid Khodabandehloo <hamidkhbl@gmail.com>
2020-07-07 19:51:14 -0600

    map model to table

---
+++ data-modeling/app.py
@@ -0,0 +1,23 @@
+#mapping between tables and classes
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test4Echo@localhost:5432/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Person ID: {self.id}, name: {self.name}>'
+
+db.create_all()
+
+@app.route('/')
+def index():
+    person = Person.query.first()
+    return person.name
