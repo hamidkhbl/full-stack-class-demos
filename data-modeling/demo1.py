4f4a5934c1c2b4e85d7f5d61ef651157d0d72dbf
Hamid Khodabandehloo <hamidkhbl@gmail.com>
2020-07-07 18:30:20 -0600

    Initial commit

---
+++ data-modeling/demo1.py
@@ -0,0 +1,25 @@
+# using psycopg2
+import psycopg2
+
+connection = psycopg2.connect('dbname=postgres user=postgres password=test4Echo')
+
+cursor = connection.cursor()
+
+cursor.execute(
+    '''
+    CREATE TABLE table3 (
+        id INTEGER PRIMARY KEY,
+        Completed BOOLEAN
+    );
+    '''
+)
+
+cursor.execute(
+    '''
+    INSERT INTO table3 VALUES(1,False)
+    '''
+)
+
+connection.commit()
+cursor.close()
+connection.close()
