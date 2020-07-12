cd3848405e68687be7e9659973aca39b0c325476
Hamid Khodabandehloo <hamidkhbl@gmail.com>
2020-07-12 17:02:10 -0600

    add delete option

--- todoapp/app.py
+++ todoapp/app.py
@@ -13,9 +13,17 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     description = db.Column(db.String(), nullable = False)
     completed = db.Column(db.Boolean, nullable = False, default = False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolist.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
+
+class TodoList(db.Model):
+    __tablename__= 'todolist'
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
 #db.create_all()
 
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
@@ -30,6 +38,17 @@ def set_complete_todo(todo_id):
     finally:
         db.session.close()
     return redirect(url_for('index'))
+
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+  try:
+    Todo.query.filter_by(id=todo_id).delete()
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return jsonify({ 'success': True })
 
 
 @app.route('/')
